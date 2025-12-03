c8dd96d2763e924133f08396731d4f339844fc43
Ranjeet Kumar <rksingh.gzp@gmail.com>
2025-12-03 19:31:16 +0530

    deployment

--- backend/uploads/admin_old.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import UploadedFile
-
-@admin.register(UploadedFile)
-class UploadedFileAdmin(admin.ModelAdmin):
-    list_display = ("original_filename", "public_id", "resource_type", "file_format", "bytes", "created_at", "uploaded_by")
-    search_fields = ("original_filename", "public_id")
